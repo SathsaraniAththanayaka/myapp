aff409f1a737248e530173460142600ca2d67616
SathsaraniAththanayaka <e18030@eng.pdn.ac.lk>
2023-09-11 17:06:11 +0530

    commit 1

---
+++ src/components/TopicBox.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./topicbox.css"
+
+export default function TopicBox() {
+  return (
+    <div>
+        <div className="topicBox">
+            <h1>Topic box</h1>
+        </div>
+    </div>
+  )
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import TopicBox from '../components/TopicBox'
+
+export default function Home() {
+  return (
+    <div>
+        <h1>This is my home</h1>
+        <TopicBox/>
+    </div>
+  )
+}
